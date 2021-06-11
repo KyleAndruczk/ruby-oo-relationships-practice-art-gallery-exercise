168bce4f4c50cd1c01ac5108838d9c64c1b0778a
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-06-11 12:28:20 -0400

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,32 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+
+  def paintings
+      Painting.all.select { |painting| painting.gallery == self }
+  end
+
+  def artists
+      paintings.map { |painting| painting.artist }.uniq
+  end
+
+  def artist_names
+      artists.map { |artist| artist.name }.uniq
+  end
+  
+  def most_expensive_painting
+      max_priced_painting = paintings.max {|painting1, painting2| painting1.price <=> painting2.price}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,24 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Van Gogh", 1)
+a2 = Artist.new("Kahlo", 2)
+a3 = Artist.new("Picasso", 2)
+
+
+g1 = Gallery.new("MoMA", "New York")
+g2 = Gallery.new("National Gallery", "Washington, DC")
+
+
+
+Painting.new("Starry Night", 100, a1, g1)
+Painting.new("Self Portrait", 200, a1, g2)
+Painting.new("The Night Café", 250, a1, g2)
+Painting.new("The Two Fridas", 300, a2, g1)
+Painting.new("The Broken Column", 400, a2, g2)
+Painting.new("The Weeping Woman", 450, a3, g1)
+a2.create_painting("Self Portrait", 475, g2)
+a3.create_painting("Les Demoiselles d'Avignon", 500, g1)
+a3.create_painting("Guernica", 600, g2)
 
 
 
